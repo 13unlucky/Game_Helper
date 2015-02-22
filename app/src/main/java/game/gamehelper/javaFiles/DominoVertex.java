38768562db604f895929720827295c5ea26e0782
ListsOfArrays <ListsOfArrays@users.noreply.github.com>
2015-02-21 21:19:04 -0500

    refactored code for brute force

--- app/src/main/java/game/gamehelper/javaFiles/DominoVertex.java
+++ app/src/main/java/game/gamehelper/javaFiles/DominoVertex.java
@@ -7,10 +7,12 @@ package game.gamehelper.javaFiles;
 public class DominoVertex {
     private final int MAX_EDGE;
     private boolean edgeList[];
+    private int numEdges;
 
     DominoVertex(int highestDouble) {
         MAX_EDGE = highestDouble;
         edgeList = new boolean[MAX_EDGE + 1];
+        numEdges = 0;
     }
 
     /**
@@ -20,6 +22,7 @@ public class DominoVertex {
     public void addEdge(int edgeNum) {
         try {
             edgeList[edgeNum] = true;
+            numEdges++;
         }
         catch (ArrayIndexOutOfBoundsException e) {
             e.printStackTrace();
@@ -32,6 +35,10 @@ public class DominoVertex {
      */
     public void toggleEdge(int edgeNum) {
         try {
+            if (edgeList[edgeNum])
+                numEdges--;
+            else
+                numEdges++;
             edgeList[edgeNum] = !edgeList[edgeNum];
         }
         catch (ArrayIndexOutOfBoundsException e) {
@@ -49,6 +56,7 @@ public class DominoVertex {
             return edgeList[edgeNum];
         }
         catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
             return false;
         }
     }
@@ -59,5 +67,10 @@ public class DominoVertex {
      */
     public boolean[] dumpEdges() {
         return edgeList.clone();
+    }
+
+    //returns the edge num for this vertex.
+    public int getEdgeNum() {
+        return numEdges;
     }
 }
